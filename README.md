0cf091fb830b60d8cc0aa3dd922fcbb135c7d00d
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2024-11-26 09:28:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Nidavellir Mjolnir
---
+++ src/ThorServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Nidavellir\Thor;
+
+use Illuminate\Support\ServiceProvider;
+
+class ThorServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+    }
+}
