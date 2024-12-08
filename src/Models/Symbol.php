9574e465f39e7c37593969fa8edce52402512edd
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2024-12-08 16:49:24 +0100

    Update Symbol.php

--- src/Models/Symbol.php
+++ src/Models/Symbol.php
@@ -3,11 +3,13 @@
 namespace Nidavellir\Thor\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Nidavellir\Mjolnir\Concerns\Models\Symbol\HasApiFeatures;
 
 class Symbol extends Model
 {
+    use HasApiFeatures;
+
     protected $casts = [
-        'is_active' => 'boolean',
         'indicator_last_synced_at' => 'datetime',
     ];
 
