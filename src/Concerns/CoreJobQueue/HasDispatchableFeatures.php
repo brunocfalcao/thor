cade37006042609a6e7b1e3f4a402e23cfec807e
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2025-01-12 12:54:25 +0000

    Update HasDispatchableFeatures.php

--- src/Concerns/CoreJobQueue/HasDispatchableFeatures.php
+++ src/Concerns/CoreJobQueue/HasDispatchableFeatures.php
@@ -72,6 +72,7 @@ trait HasDispatchableFeatures
                     ->get();
 
                 if ($jobsToDispatch->isEmpty()) {
+                    info('No jobs to dispatch');
                     break; // No jobs to dispatch for this index
                 }
 
@@ -88,6 +89,7 @@ trait HasDispatchableFeatures
                     if ($job->queue == 'sync') {
                         $jobInstance->handle();
                     } else { // Dispatch the job
+                        info('Queueing ' . $job->id);
                         Queue::pushOn($job->queue, $jobInstance);
                     }
                 }
