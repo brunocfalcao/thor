c988c700fb98dd47237f85ef0a3aa465ee6daad1
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2025-01-24 00:51:45 +0100

    wip

--- database/seeders/SchemaSeeder1.php
+++ database/seeders/SchemaSeeder1.php
@@ -335,7 +335,7 @@ class SchemaSeeder1 extends Seeder
             'api_system_id' => $binance->id,
 
             'minimum_balance' => 500,
-            'max_concurrent_trades' => 1,
+            'max_concurrent_trades' => 12,
             'margin_override' => 100,
             'position_size_percentage' => 2,
             'max_leverage' => 20,
