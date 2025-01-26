6648c3c0d30988cc40f691149d97c26d962755e5
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2025-01-26 23:14:46 +0100

    wip

---
+++ database/migrations/2025_01_26_000002_update_schema_6.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Str;
+use Nidavellir\Thor\Models\Account;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    // FK'S BOOLEANS INTS/NUMERICS STRINGS ARRAYS/JSONS TEXTS DATETIMES
+    public function up(): void
+    {
+        Schema::dropIfExists('application_logs');
+
+        Schema::create('application_logs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('loggable_id')->nullable();
+            $table->string('loggable_type')->nullable();
+            $table->string('block_uuid')->nullable();
+            $table->string('action_canonical')->nullable();
+            $table->text('action')->nullable();
+            $table->text('return_value')->nullable();
+            $table->longText('return_data')->nullable();
+            $table->text('comments')->nullable();
+            $table->json('debug_data')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::table('accounts', function (Blueprint $table) {
+            $table->uuid()
+                ->after('quote_id')
+                ->nullable()
+                ->comment('Account UUID');
+        });
+
+        /**
+         * Generate UUIDs for each account.
+         */
+        Account::all()->each(function ($account) {
+            $account->uuid = (string) Str::uuid();
+            $account->save();
+        });
+    }
+};
