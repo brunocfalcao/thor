2951df97bec2d1b99c1b968161307d4868ae9d35
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2025-01-26 23:17:06 +0100

    wip

--- database/migrations/2025_01_26_000002_update_schema_6.php
+++ database/migrations/2025_01_26_000002_update_schema_6.php
@@ -1,10 +1,10 @@
 <?php
 
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Str;
 use Nidavellir\Thor\Models\Account;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
