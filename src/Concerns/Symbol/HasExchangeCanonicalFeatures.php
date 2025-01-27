c1b0e63216a94ff7b2a2721bf97d7ee1901c186d
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2025-01-27 04:34:40 +0100

    wip

--- src/Concerns/Symbol/HasExchangeCanonicalFeatures.php
+++ src/Concerns/Symbol/HasExchangeCanonicalFeatures.php
@@ -6,6 +6,16 @@ use Nidavellir\Thor\Models\ApiSystem;
 
 trait HasExchangeCanonicalFeatures
 {
+    public function exchangeCanonical(ApiSystem $apiSystem)
+    {
+        if ($this->exchange_canonicals != null && array_key_exists($apiSystem->canonical, $this->exchange_canonicals)) {
+            return $this->exchange_canonicals[$apiSystem->canonical];
+        }
+
+        return $this->token;
+    }
+
+    /*
     public function getExchangeCanonicalAttribute(ApiSystem $apiSystem)
     {
         if (array_key_exists($apiSystem->canonical, $this->exchange_canonicals)) {
@@ -14,4 +24,5 @@ trait HasExchangeCanonicalFeatures
 
         return $this->token;
     }
+    */
 }
