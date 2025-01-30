825c5e2734b30893a4f26b408aa0e15d58ae0abb
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2025-01-30 23:35:32 +0100

    Update OrderHistory.php

--- src/Models/OrderHistory.php
+++ src/Models/OrderHistory.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderHistory extends Model
 {
+    protected $table = 'order_history';
+
     public function order()
     {
         return $this->belongsTo(Order::class);
