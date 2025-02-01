e549c16216170c902bfb4838d34975949cdd2093
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2025-02-01 09:39:33 +0100

    Create 2025_02_01_000001_update_schema_8.php

---
+++ database/migrations/2025_02_01_000001_update_schema_8.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    // FK'S BOOLEANS INTS/NUMERICS STRINGS ARRAYS/JSONS TEXTS DATETIMES
+    public function up(): void
+    {
+        Schema::table('api_requests_log', function (Blueprint $table) {
+
+            $table->unsignedInteger('duration')
+                  ->nullable()
+                  ->after('debug_data');
+
+            $table->timestamp('started_at')
+                  ->nullable()
+                  ->after('hostname');
+
+            $table->timestamp('completed_at')
+                  ->nullable()
+                  ->after('started_at');
+        });
+    }
+};
