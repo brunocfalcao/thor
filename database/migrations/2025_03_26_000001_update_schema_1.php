d9adb5e8c844e1a37441ad34061fb88e57054670
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2025-03-26 18:42:15 +0100

    Create 2025_03_26_000001_update_schema_1.php

---
+++ database/migrations/2025_03_26_000001_update_schema_1.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('accounts', function (Blueprint $table) {
+            $table->unsignedBigInteger('last_report_id')->nullable()->after('monthly_profit_objective');
+        });
+    }
+};
